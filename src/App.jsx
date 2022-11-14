7ef6c91e3a4ee813bf25d4bd6d138ea1e5b60a1a
NqDyK2 <nguyenquyduyabc@gmail.com>
2022-11-14 23:42:30 +0700

    feat:feature remove

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,30 @@ function App() {
       id: 1,
       name: "Do housework",
       desc: "Lorem....",
-      date: "11/12/2022",
+      date: "Mon Nov 14 2022 23:40:24 GMT+0700",
       piority: "Low",
     },
     {
       id: 2,
       name: "Do homework",
       desc: "Lorem....",
-      date: "11/12/2022",
+      date: "Tue Nov 15 2022 23:40:24 GMT+0700",
       piority: "Normal",
     },
     {
       id: 3,
       name: "Learn something",
       desc: "Lorem....",
-      date: "11/12/2022",
+      date: "Fri Nov 11 2022 23:40:24 GMT+0700",
       piority: "High",
     },
   ]
   const [list, setList] = useState(listToDo);
   const [filteredResults, setFilteredResults] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  const [isChecked, setIsChecked] = useState([])
+  const jsonObj = JSON.stringify(list)
+  localStorage.setItem("jsonObj", jsonObj)
   const { register, handleSubmit, formState: { errors } } = useForm();
   const date = new Date();
   const formatDate = (date) => {
@@ -45,7 +48,6 @@ function App() {
 
     return [year, month, day].join('-');
   }
-
   const onSubmit = (data) => {
     console.log(data.piority);
     const obj = {
@@ -59,6 +61,12 @@ function App() {
     newListTodo.push(obj);
     setList(newListTodo)
   }
+  const handleRemove = (id) => {
+    const newListTask = list.filter((item) => item.id !== id);
+    setList(newListTask)
+    console.log(newListTask);
+  }
+
   const searchItem = (searchValue) => {
     setSearchInput(searchValue);
     if (searchInput !== '') {
@@ -70,90 +78,116 @@ function App() {
       setFilteredResults(list)
     }
   }
+
+  const handleClick = (value) => {
+    console.log(value);
+  }
   return (
     <>
-      <div>
-        <h1 className='title'>New Task</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" {...register('name', { required: true })} placeholder='Add new task' />
-          {errors.name && <><br /> <span>Nhập đi đã.</span> </>}
-          <label htmlFor="">Description</label>
-          <textarea {...register('desc')} />
-          <div>
+      <main>
+        <div>
+          <h1 className='title'>New Task</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" {...register('name', { required: true })} placeholder='Add new task' />
+            {errors.name && <><br /> <span>Nhập đi đã.</span> </>}
+            <label htmlFor="">Description</label>
+            <textarea {...register('desc')} />
             <div>
-              <label htmlFor="" >Due Date</label>
-              <input type="date" {...register('date')} min={formatDate(date)} defaultValue={formatDate(date)} />
+              <div>
+                <label htmlFor="" >Due Date</label>
+                <input type="date" {...register('date')} min={formatDate(date)} defaultValue={formatDate(date)} />
+              </div>
+              <div>
+                <label htmlFor="">Piority</label>
+                <select {...register('piority')} defaultValue="Normal" >
+                  <option value="Low">Low</option>
+                  <option value="Normal">Normal</option>
+                  <option value="High">High</option>
+                </select>
+              </div>
             </div>
-            <div>
-              <label htmlFor="">Piority</label>
-              <select {...register('piority')} defaultValue="Normal" >
-                <option value="Low">Low</option>
-                <option value="Normal">Normal</option>
-                <option value="High">High</option>
-              </select>
-            </div>
-          </div>
-          <button> Add </button>
-        </form>
-      </div>
-      <div>
-        <h1 className='title'>To Do List</h1>
-        <input type="text" onChange={(e) => searchItem(e.target.value)} />
-        {
-          searchInput.length > 1 ? (
-            filteredResults.map((item) => (
-              <>
-                <input type="checkbox" />
-                <Collapsible type='checkbox' key={item.id} trigger={`${item.name}`}>
-
-                  <form>
-                    <input placeholder={item.name} id="name" />
-                    <span>Description</span>
-                    <textarea id="desc" />
-                    <div>
-                      <div>
-                        <label htmlFor="">Due Date</label>
-                        <input type="date" defaultValue={item.date} />
-                      </div>
-                      <div>
-                        <label htmlFor="">Piority</label>
-                        <select defaultValue={item.piority}>
-                          <option value="Low">Low</option>
-                          <option value="Normal">Normal</option>
-                          <option value="High">High</option>
-                        </select>
-                      </div>
+            <button> Add </button>
+          </form>
+        </div>
+        <div>
+          <h1 className='title'>To Do List</h1>
+          <input type="text" onChange={(e) => searchItem(e.target.value)} />
+          {
+            searchInput.length > 1 ? (
+              filteredResults.map((item) => (
+                <>
+                  <div key={item.id}>
+                    <div >
+                      <input type="checkbox" onClick={() => handleClick(item.id)} />
+                      <Collapsible type='checkbox' key={item.id} trigger={`${item.name}`}>
+                        <form>
+                          <input placeholder={item.name} id="name" />
+                          <span>Description</span>
+                          <textarea id="desc" />
+                          <div>
+                            <div>
+                              <label htmlFor="">Due Date</label>
+                              <input type="date" defaultValue={formatDate(item.date)} />
+                            </div>
+                            <div>
+                              <label htmlFor="">Piority</label>
+                              <select defaultValue={item.piority}>
+                                <option value="Low">Low</option>
+                                <option value="Normal">Normal</option>
+                                <option value="High">High</option>
+                              </select>
+                            </div>
+                          </div>
+                          <button>Update</button>
+                        </form>
+                      </Collapsible>
                     </div>
-                    <button>Update</button>
-                  </form>
-                </Collapsible>
-              </>
-            ))
-          ) : (list.map((item, index) => (
-            <Collapsible type='checkbox' key={item.id} trigger={`${item.name}`}>
-              <form>
-                <input placeholder={item.name} id="name" />
-                <span>Description</span>
-                <textarea id="desc" />
-                <div>
-                  <div>
-                    <label htmlFor="">Due Date</label>
-                    <input type="date" defaultValue={item.date} />
+                    <div>
+                      <button>Detail</button>
+                      <button onClick={() => handleRemove(item.id)}>Remove</button>
+                    </div>
                   </div>
+
+                </>
+              ))
+            ) : (list.map((item, index) => (
+              <>
+                <div key={item.id}>
                   <div>
-                    <label htmlFor="">Piority</label>
-                    <select defaultValue={item.piority}>
-                      <option value="Low">Low</option>
-                      <option value="Normal">Normal</option>
-                      <option value="High">High</option>
-                    </select>
+                    <input type="checkbox" />
+                    <Collapsible type='checkbox' key={item.id} trigger={`${item.name}`}>
+                      <form>
+                        <input placeholder={item.name} id="name" />
+                        <span>Description</span>
+                        <textarea id="desc" />
+                        <div>
+                          <div>
+                            <label htmlFor="">Due Date</label>
+                            <input type="date" defaultValue={item.date} />
+                          </div>
+                          <div>
+                            <label htmlFor="">Piority</label>
+                            <select defaultValue={item.piority}>
+                              <option value="Low">Low</option>
+                              <option value="Normal">Normal</option>
+                              <option value="High">High</option>
+                            </select>
+                          </div>
+                        </div>
+                        <button>Update</button>
+                      </form>
+                    </Collapsible>
+                  </div>
+
+                  <div>
+                    <button>Detail</button>
+                    <button onClick={() => handleRemove(item.id)}>Remove</button>
                   </div>
                 </div>
-                <button>Update</button>
-              </form>
-            </Collapsible>)))
-        }
-      </div>
+              </>)))
+          }
+        </div>
+      </main>
     </>
   )
 }
